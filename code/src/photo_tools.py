483fc0c33f78779119a53d470a1aff8b14a9c96b
Erick Medeiros Anastácio <erickfis@gmail.com>
2024-05-21 22:37:16 -0300

    dev archive_files_by_name

--- code/src/photo_tools.py
+++ code/src/photo_tools.py
@@ -23,7 +23,7 @@ def get_os_creation_time(file: Path) -> datetime:
 
 def archive_files(
     *, source_folder: str, destination_folder: str,
-    agg_lvl: str ='day'
+    agg_lvl: str ='day', copy_first=True
 ) -> None:
     """Scans files in folder and move each one to a subfolder.
 
@@ -34,9 +34,14 @@ def archive_files(
     folders.
     """
     # copy files
-    shutil.copytree(source_folder, destination_folder, dirs_exist_ok=True)
+    if copy_first:
+        print('Copying first')
+        shutil.copytree(source_folder, destination_folder, dirs_exist_ok=True)
+        folder = Path(destination_folder)
+    else:
+        print('not creating a copy')
+        folder = Path(source_folder)
     # start scanning new folder
-    folder = Path(destination_folder)
     all_files = list(folder.glob('*'))
     print(f'{len(all_files)} files to be moved.')
 
@@ -78,13 +83,19 @@ def sort_rename_files(*, source_folder: str, destination_folder: str) -> None:
 
 def archive_files_by_name(
     *, source_folder: str, destination_folder: str,
-    agg_lvl: str ='day'
+    agg_lvl: str = 'month', copy_first=True
 ):
     """TODO."""
     # copy files
-    shutil.copytree(source_folder, destination_folder, dirs_exist_ok=True)
-    folder = pathlib.Path(destination_folder)
-    all_files = list(folder.glob('*'))
+    if copy_first:
+        print('Copying first')
+        shutil.copytree(source_folder, destination_folder, dirs_exist_ok=True)
+        folder = Path(destination_folder)
+    else:
+        print('not creating a copy')
+        folder = Path(source_folder)
+
+    all_files = list(folder.glob('IMG*'))
     print(f'{len(all_files)} files to be moved')
 
     if agg_lvl == 'day':
@@ -93,15 +104,17 @@ def archive_files_by_name(
         print('Monthly aggregation')
 
     for file in tqdm(all_files):
-        ymd = file.name.split('_')[1]
+        ymd = file.name.split('IMG')[1]
         year = ymd[:4]
         month = ymd[4:6]
-        day = ymd[6:]
-        if agg_lvl != 'day':
-            dest_folder = f'{year}-{month}'
+        day = ymd[6:8]
+        if agg_lvl == 'day':
+            date = f'{year}-{month}-{day}'
         else:
-            dest_folder = f'{year}-{month}-{day}'
-        print(dest_folder)
-        dest = file.parent / dest_folder / file.name
-        os.makedirs(dest.parent, exist_ok=True)
-        file.rename(dest)
+            date = f'{year}-{month}'
+
+        dest_folder = file.parent / date
+        dest_folder.mkdir(parents=True, exist_ok=True)
+        dest_file = dest_folder / file.name
+        
+        file.rename(dest_file)
